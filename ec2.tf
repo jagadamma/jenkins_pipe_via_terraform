0a800b6bf6009b9ee47724d485fc4beb380feaf7
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-05-17 14:37:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,22 @@
 resource "aws_instance" "my1" {
   ami = "ami-0fc841be1f929d7d1"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.example_subnet_1.id
+  subnet_id     = aws_subnet.pub_subnet-1
   security_groups = aws_security_group.sg.id
+  
+  tags = {
+    Name = "Instance1"
+  }
 
-  ]
+  
 resource "aws_instance" "my2" {
   ami           = "ami-xxxxxxxxxxxxxx"  
   instance_type = "t2.micro" 
-  subnet_id     = aws_subnet.example_subnet_2.id
+  subnet_id     = aws_subnet.pub_subnet-2
   security_group_ids =  aws_security_group.sg.id
 
   tags = {
-    Name = "ExampleInstance2"
+    Name = "Instance2"
   }
 }  
 
