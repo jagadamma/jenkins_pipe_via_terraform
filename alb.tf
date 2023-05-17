7364daa79f41bbbf5630624f2f0b29e5a49d3b64
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-05-17 14:44:44 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,6 +1,8 @@
 resource "aws_lb" "lb" {
   name               = "ALB"
   internal           = false
-  load_balancer_type = "network"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [aws_subnet.pub_subnet-1.id,aws_subnet.pub_subnet-2.id]
+  
 }                      
